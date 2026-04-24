c0a71ded95219d331643351979aeccb653d17b1b
vaishnavithulir <ksvaishnavidevibca@gmail.com>
2026-04-24 12:27:21 +0530

    Switch from Static to Cloud mode

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   devIndicators: {
     // In Next.js 15+, this property is renamed to 'position'
     position: "bottom-right",
